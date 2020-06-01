80425722cba9d66ffa1331a7c75ba608b2559b57
Emma Hellman <ehh801@icloud.com>
2020-06-01 14:02:56 -0700

    co-author: Tristan Emmerson

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+let sentence = prompt("Enter a sentence");
+
+function capitalize (sentence) {
+	const firstLetter = sentence.charAt(0);
+  const uppercaseFirstLetter = firstLetter.toUpperCase();
+  const length = sentence.length;
+  const lastLetter = sentence.charAt(length - 1);
+  const uppercaseLastLetter = lastLetter.toUpperCase();
+  const wholeSentence = uppercaseLastLetter + sentence.slice(1, length - 1) + uppercaseFirstLetter;
+  return alert(wholeSentence);
+}
+
+capitalize(sentence); 
+
+let sentence = prompt("Enter a sentence");
+function concat (sentence) {
+const length = sentence.length;
+const divide = Math.floor(length / 2);
+const character = sentence.charAt(divide);
+return alert(character + sentence);
+}
